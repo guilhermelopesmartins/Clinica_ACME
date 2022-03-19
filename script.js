1f7ff12b012ee055b3b088c914ebf63a88090402
guilhermelopesmartins <guirockmartins@hotmail.com>
2022-03-19 00:57:06 -0300

    Trabalhando com localstorage
    Comecei a armazenar com o localstorage os dados preenchidos e a coloca-los em uma tabela. Concertei alguns erros e outros acabaram sendo criados

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
-let clientes = document.getElementById('clientes')
+let res = document.getElementById('patient')
 
 function Register(){
 
 //Pegando as informações da área de cadastro
 let name = document.getElementById('txtName').value
-let date = document.getElementById('date').valeu
+let date = document.getElementById('date').value
 let sex = document.querySelector('input[name="radioSex"]:checked').value
 let adress = document.getElementById('txtAdress').value
-let txtCPF = document.getElementById('txtCPF')
-let cpf = Number(txtCPF.valeu)
+let cpf = document.getElementById('txtCPF').value
 var status = document.getElementById('status').selectedOptions[0].value;
 
-clientes.innerHTML = `O ${name} que nasceu em ${date} do sexo ${sex} que mora na ${adress} tem o CPF: ${cpf} e está ${status} como cliente`
+// Cria paciente como um objeto e salva no localstorage com o nome como key para facilitar pesquisa
+let patient = {nome: name, data: date, sexo: sex, endereco: adress, CPF: cpf, stats: status}
+localStorage.setItem(localStorage.length, JSON.stringify(patient))
 
+
+// Preciso procurar atualizar a tabela automaticamente com React
+AtualizarTabela()
+}
+
+function AtualizarTabela(){
+    //let tr = document.getElementById('tr')
+    let table = document.getElementById('table')
+
+    for (let index = 0; index < localStorage.length; index++) {
+        // Recriar os dados como objeto novamente
+        let txtPatient = localStorage.getItem(index);
+        let patient = JSON.parse(txtPatient)
+        
+        // Cria uma nova tr para mostrar os dados de um paciente
+        let tr = document.createElement('tr')
+        table.appendChild(tr)
+
+        // Adiciona td's com as informações ao tr criado
+        let name = document.createElement('td')
+        name.innerText = patient.nome
+        tr.appendChild(name)
+    }
 }
