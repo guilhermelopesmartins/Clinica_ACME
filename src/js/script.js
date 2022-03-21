64690884476907adda5c95ad0ace76b752b473ce
guilhermelopesmartins <guirockmartins@hotmail.com>
2022-03-21 14:04:47 -0300

    Finalização
    Modifiquei pequenas questões da design, melhorei um pouco as verificações dos dados, adicionei o recurso de cancelar a edição e aperfeiçoei um pouco a organização do código.

--- src/js/script.js
+++ src/js/script.js
@@ -3,31 +3,21 @@ window.onload = UpdateTable; // Atualiza a tabela ao atualizar página
 let editable = null // Essa variavel vai mostrar se os dados irão ser editados ou cadastrados
 
 function Save(){
-    // Verificações sobre o preenchimento de dados
-    if(document.getElementById('txtName').value == ''){
-        window.alert('Digite o nome para continuar!')
+
+    if (GetValidation() == false)
         return
-    }
-    if(document.getElementById('date').value == ''){
-        window.alert('Digite a data para continuar!')
-        return
-    }
-    if(document.getElementById('txtCPF').value == ''){
-        window.alert('Digite o CPF para continuar!')
-        return
-    }
-    
+
     if(editable == null){
-    // Verifica se o CPF é repetido ao cadastrar
-    for (let index = 0; index < localStorage.length; index++) {
-        let txtPatient = localStorage.getItem(index)
-        let patient = JSON.parse(txtPatient)
-            
-        if (patient.CPF == document.getElementById('txtCPF').value) {
-            window.alert('Esse CPF já foi cadastrado, por favor revise os dados inseridos...')
-            return
+        // Verifica se o CPF é repetido ao cadastrar
+        for (let index = 0; index < localStorage.length; index++) {
+            let txtPatient = localStorage.getItem(index)
+            let patient = JSON.parse(txtPatient)
+                
+            if (patient.CPF == document.getElementById('txtCPF').value) {
+                window.alert('Esse CPF já foi cadastrado, por favor revise os dados inseridos...')
+                return 
+            }
         }
-    }
         Register()
     }else{
         // Verifica se o CPF é repetido ao Editar
@@ -48,20 +38,21 @@ function Save(){
     CleanAll()
 }
 
-// Inativa ou Ativa o paciente ao clicar no seu status
-function Inactivate(n){
-    let txtPatient = localStorage.getItem(n)
-    let patient = JSON.parse(txtPatient)
 
-    var indexSearch = txtPatient.indexOf('Ativo');
-
-    if (indexSearch != -1) {    
-        patient.stats = 'Inativo'
-    }else{
-        patient.stats = 'Ativo'
+// Verificações sobre o preenchimento de dados
+function GetValidation (){
+    if(document.getElementById('txtName').value.trim() == ''){
+        window.alert('Digite o nome para continuar!' + String.prototype.trim(document.getElementById('txtName').value)+ 'assim')
+        return false
+    }   
+    if(document.getElementById('date').value == ''){
+        window.alert('Digite a data para continuar!')
+        return false
     }
-    localStorage.setItem(n, JSON.stringify(patient))
-    UpdateTable()
+    if(document.getElementById('txtCPF').value == '' || document.getElementById('txtCPF').value.length < 11){
+        window.alert('Digite o CPF para continuar!')
+        return false
+    }
 }
 
 
@@ -76,7 +67,7 @@ let cpf = document.getElementById('txtCPF').value
 var status = document.getElementById('status').selectedOptions[0].value
 
 // Cria paciente como um objeto e salva no localstorage como string
-let patient = {nome: name, data: date, sexo: sex, endereco: adress, CPF: cpf, stats: status}
+let patient = {nome: name, data: date, sexo: sex, endereco: adress, CPF: cpf, stats: status.toUpperCase()}
 localStorage.setItem(localStorage.length, JSON.stringify(patient))
 
 
@@ -121,7 +112,7 @@ function UpdateTable(){
         cpf.innerText = patient.CPF
         tr.appendChild(cpf)
         let status = document.createElement('td')
-        status.innerText = patient.stats
+        status.innerText = '\u{270D}' + patient.stats
         status.setAttribute('onclick', `Inactivate(${index})`)
         tr.appendChild(status)
 
@@ -142,6 +133,9 @@ function PreEdit(n){
     let btn = document.getElementById('btnSave')
     btn.innerText = 'Atualizar'
 
+    // Mostra o botão que possibilita cancelar a edição
+    document.getElementById('btnCancel').style.display = 'inline'
+
     editable = n
 
     // Inseri os valores do paciente nos campos
@@ -149,7 +143,8 @@ function PreEdit(n){
     document.getElementById('date').value = patient.data
     document.getElementById('txtAdress').value = patient.endereco
     document.getElementById('txtCPF').value = patient.CPF
-    if (patient.stats == 'Ativo')
+    document.getElementById('status').getElementsByTagName('option')[1].disabled = false
+    if (patient.stats == 'ATIVO')
         document.getElementById('status').getElementsByTagName('option')[0].selected = 'selected'
     else
         document.getElementById('status').getElementsByTagName('option')[1].selected = 'selected'
@@ -174,7 +169,7 @@ function Edit(n){
     var status = document.getElementById('status').selectedOptions[0].value
 
     // Atualiza a memoria com os novos valores e atualiza a tabela
-    patient = {nome: name, data: date, sexo: sex, endereco: adress, CPF: cpf, stats: status}
+    patient = {nome: name, data: date, sexo: sex, endereco: adress, CPF: cpf, stats: status.toUpperCase()}
     localStorage.setItem(n, JSON.stringify(patient))
 
     UpdateTable()
@@ -187,6 +182,7 @@ function SearchByName(){
     let search = document.getElementById('txtSearch').value
     let table = document.getElementById('table')
     
+    // Limpa a tabela
     while (table.hasChildNodes()) {
         table.removeChild(table.lastChild);
     }
@@ -195,7 +191,7 @@ function SearchByName(){
         let txtPatient = localStorage.getItem(index)
         let patient = JSON.parse(txtPatient)
 
-
+        // Faz a pesquisa pelo nome
         if(!patient.nome.search(search)){
 
             let tr = document.createElement('tr')
@@ -232,12 +228,35 @@ function SearchByName(){
     }
 }
 
+// Inativa ou Ativa o paciente ao clicar no seu status
+function Inactivate(n){
+    let txtPatient = localStorage.getItem(n)
+    let patient = JSON.parse(txtPatient)
+
+    var indexSearch = txtPatient.indexOf('ATIVO');
+
+    if (indexSearch != -1)    
+        patient.stats = 'INATIVO'
+
+    localStorage.setItem(n, JSON.stringify(patient))
+    UpdateTable()
+}
+
 function CleanAll(){
+    // Limpa campos de texto
     document.getElementById('txtName').value = ''
     document.getElementById('date').value = ''
     document.getElementById('txtAdress').value = ''
     document.getElementById('txtCPF').value = ''
 
+    // Seleciona opição ativo e desabilita opção inativo
+    document.getElementById('status').getElementsByTagName('option')[0].selected = 'selected'
+    document.getElementById('status').getElementsByTagName('option')[1].disabled = true
+
+    // Esconde botão cancelar edição
+    document.getElementById('btnCancel').style.display = 'none'
+
+    // Desbilita a funcionalidade de editar
     let btn = document.getElementById('btnSave')
     btn.innerText = 'Cadastrar'
     editable = null
